80b5c3c358a2bf0695ea80f87c37be99a6050847
KasperAPedersen <kasperpedersen1999@gmail.com>
2025-05-28 12:30:29 +0200

    Remove `Dashboard` component and update styles:
    - Deleted `Dashboard.js` and its references in `App.js` to streamline routing.
    - Updated global and layout-specific styles in `index.css`, `Sidebar.css`, and `Header.css` for improved visuals.
    - Refined routes to use temporary dashboard placeholders for

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Dashboard from "./components/Dashboard/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import SessionProvider, { useSession } from "./context/SessionProvider";
 import Header from "./components/Header/Header";
@@ -20,8 +19,8 @@ function App() {
                         <Header />
                         <section className="dashboard-container">
                             <Routes>
-                                <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
-                                <Route path="/dashboard-test" element={<ProtectedRoute element={<h1>test</h1>} />} />
+                                <Route path="/dashboard" element={<ProtectedRoute element={<h1>Dashboard 1</h1>} />} />
+                                <Route path="/dashboard-test" element={<ProtectedRoute element={<h1>Dashboard 2</h1>} />} />
                             </Routes>
                         </section>
 
@@ -35,7 +34,7 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard />} />} />
+                    <Route path="/dashboard" element={<ProtectedRoute />} />
                 </Routes>
             </>
         );
