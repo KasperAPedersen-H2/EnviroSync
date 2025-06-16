b1ac555f3503e2035fb621f6c5d8451e3adce139
KasperAPedersen <kasperpedersen1999@gmail.com>
2025-06-16 18:20:32 +0200

    Add mobile-friendly links to `Dropdown` and cleanup unused mobile `Sidebar` styles:
    - Added dynamic links (`Dashboard`, `Management`) for mobile users in the `Dropdown` menu.
    - Implemented window resize handler in `Dropdown` for responsive adjustments.
    - Removed unused media query styles from `Sidebar` to improve maintainability.

--- client/src/components/Header/Dropdown/Dropdown.js
+++ client/src/components/Header/Dropdown/Dropdown.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { AccountCircle, Settings, Logout } from "@mui/icons-material";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
 import "./Dropdown.css";
 
 const Dropdown = ({ username, avatarData }) => {
     const [dropdownVisible, setDropdownVisible] = useState(false);
     const dropdownRef = useRef(null);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const toggleDropdown = () => {
         setDropdownVisible((prevVisible) => !prevVisible);
@@ -23,6 +26,15 @@ const Dropdown = ({ username, avatarData }) => {
         }
         return "img_avatar3.png";
     }
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     useEffect(() => {
         if (dropdownVisible) {
@@ -47,6 +59,19 @@ const Dropdown = ({ username, avatarData }) => {
             <div className={`dropdown-menu ${dropdownVisible ? "visible" : ""}`}>
                 <h3>Welcome, {username}</h3>
                 <nav>
+                    { windowWidth <= 768 && (
+                        <>
+                            <Link to="/" className="sidebar-link">
+                                <DashboardIcon />
+                                Dashboard
+                            </Link>
+                            <Link to="/manage" className="sidebar-link">
+                                <DashboardCustomizeIcon />
+                                Management
+                            </Link>
+                        </>
+                    )}
+
                     <Link to="/profile">
                         <AccountCircle />
                         Account
