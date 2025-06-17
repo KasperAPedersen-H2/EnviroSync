276586d3e2b64b2c3b659a8efa5cc5b7398a6f35
Kasper Alexander Pedersen <35560445+KasperAPedersen@users.noreply.github.com>
2025-06-17 12:58:02 +0200

    Update sensor.js

--- server/routes/sensor.js
+++ server/routes/sensor.js
@@ -51,28 +51,6 @@ router.post('/data/new', async (req, res) => {
         console.log(e);
         res.status(500).json({ message: 'Internal server error' });
     }
-
-    /*let { sn, temp, humidity, pressure, tvoc} = req.body;
-
-    if (!sn ) {
-        return res.status(400).json({ message: 'Missing required fields' });
-    }
-
-    if (isNaN(temp) || isNaN(humidity) || isNaN(pressure) || isNaN(tvoc)) {
-        return res.status(400).json({ message: 'Invalid data' });
-    }
-
-    let checkSN = await Models.Devices.findOne({ where : { serial_number: sn } })
-    if (!checkSN) {
-        if(sn.length !== 6) {
-            return res.status(400).json({ message: 'Invalid serial number' });
-        }
-        await Models.Devices.create({serial_number: sn, room_id: 1, name: 'New Device'})
-    }
-
-    await Models.Data.create({device_id: sn, temperature: temp, humidity: humidity, pressure: pressure, tvoc: tvoc})
-    Socket.getIO().emit('new-data', { deviceId: sn });
-    res.end();*/
-})
+});
 
 export default router;
