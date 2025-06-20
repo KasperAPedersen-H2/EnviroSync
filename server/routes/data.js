9cb0acc40bd15e27a5d44c577814ec8282887c29
Fischer38 <nikolajfischer38@gmail.com>
2025-06-20 07:03:53 +0200

    update data

--- server/routes/data.js
+++ server/routes/data.js
@@ -20,12 +20,34 @@ router.get('/latest/:sensorId', async (req, res) => {
 router.get('/latest/avg/:roomId', async (req, res) => {
     const { roomId } = req.params;
 
-    const data = await Models.Data.findAll({
-        where: { room_id: roomId },
-        order: [
-            ['createdAt', 'DESC']
-        ]
+    const [result] = await sequelize.query(`
+        SELECT
+            rooms.id AS room_id,
+            rooms.name AS room_name,
+            ROUND(AVG(latest_data.temperature)) AS avg_temperature,
+            ROUND(AVG(latest_data.humidity)) AS avg_humidity,
+            ROUND(AVG(latest_data.pressure)) AS avg_pressure,
+            ROUND(AVG(latest_data.tvoc)) AS avg_co2 
+        FROM rooms
+                 JOIN devices ON rooms.id = devices.room_id
+                 JOIN sensors ON devices.sensor_id = sensors.id
+                 JOIN (
+            SELECT d1.*
+            FROM data d1
+                     JOIN (
+                SELECT sensor_id, MAX(createdAt) AS max_time
+                FROM data
+                GROUP BY sensor_id
+            ) d2 ON d1.sensor_id = d2.sensor_id AND d1.createdAt = d2.max_time
+        ) AS latest_data ON sensors.id = latest_data.sensor_id
+        WHERE rooms.id = :roomId
+        GROUP BY rooms.id, rooms.name;
+        `,{
+        replacements: { roomId: roomId },
+        type: sequelize.QueryTypes.SELECT
     });
+
+    console.log(result);
 })
 
 // Get all data from sensor
@@ -38,5 +60,32 @@ router.get('/:sensorId', async (req, res) => {
 
     return res.status(200).json(data);
 });
+
+router.get('/avg/:roomId', async (req, res) => {
+    const { roomId } = req.params;
+
+    const [result] = await sequelize.query(`
+        SELECT
+            rooms.id AS room_id,
+            rooms.name AS room_name,
+            DATE_FORMAT(DATE_SUB(data.createdAt, INTERVAL MINUTE(data.createdAt) % 5 MINUTE), '%Y-%m-%d %H:%i:00') AS time_block,
+            ROUND(AVG(data.temperature)) AS avg_temperature,
+            ROUND(AVG(data.humidity)) AS avg_humidity,
+            ROUND(AVG(data.pressure)) AS avg_pressure,
+            ROUND(AVG(data.tvoc)) AS avg_co2
+        FROM rooms
+                 JOIN devices ON rooms.id = devices.room_id
+                 JOIN sensors ON devices.sensor_id = sensors.id
+                 JOIN data ON sensors.id = data.sensor_id
+        WHERE rooms.id = roomId
+        GROUP BY rooms.id, rooms.name, time_block
+        ORDER BY time_block DESC;
+        `,{
+        replacements: { roomId: roomId },
+        type: sequelize.QueryTypes.SELECT
+    });
+
+    console.log(result);
+})
 
 export default router;
